6c157f620d3459e22bc019373ccdfbf6b7433460
Lukas Schramm <schramm98@icloud.com>
2016-07-10 13:07:17 +0200

    Initial Commit

---
+++ Beziehungsrechner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Beziehungsrechner
+//
+//  Created by Lukas Schramm on 16-07-10.
+//  Copyright © 2016 Cappricorn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Beziehungsrechner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Beziehungsrechner
+//
+//  Created by Lukas Schramm on 16-07-10.
+//  Copyright © 2016 Cappricorn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
